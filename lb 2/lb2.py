b27067a425a164b74199c33f3d9dbe57bcb6aead
Lilia <rybkalilia8@gmail.com>
2025-10-24 10:14:50 +0300

    Початковий коміт: додала лабораторну роботу №2

---
+++ lb 2/lb2.py
@@ -0,0 +1,9 @@
+mystring = "Hello, world!"
+myfloat = 10.0
+myint = 20
+if mystring == "Hello, world!":
+    print(f"String: {mystring}")
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print(f"Float: {myfloat}")
+if isinstance(myint, int) and myint == 20:
+    print(f"Integer: {myint}")
