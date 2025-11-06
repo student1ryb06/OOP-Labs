86a39f8779aa0d94e05db689f799cef473e1aaa9
Lilia <rybkalilia8@gmail.com>
2025-11-06 13:55:37 +0200

    Оновлення лб 4

--- lb 4/lb4.py
+++ lb 4/lb4.py
@@ -5,10 +5,13 @@ def list_benefits():
         "Легше повторне використання коду",
         "Можливість програмістам ділитися та поєднувати код"
     ]
+
 def build_sentence(benefit):
     return f"{benefit} є перевагою використання функцій!"
+
 def name_the_benefits_of_functions():
     list_of_benefits = list_benefits()
     for benefit in list_of_benefits:
         print(build_sentence(benefit))
+
 name_the_benefits_of_functions()
